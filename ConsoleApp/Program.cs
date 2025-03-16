d3e6e75400e563f1824748f2fa17829c1dc49e70
daniilnsdk <daniilnsdk@gmail.com>
2025-03-15 20:16:35 -0400

    NEWCONSOLE commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("**************");
+            Console.WriteLine("\nMenu:\n");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit\n");
+            Console.WriteLine("**************");
+            Console.Write("Enter your choice: ");
+
+            string? choice = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(choice))
+                continue;
+
+            if (choice == "4")
+            {
+                Console.WriteLine("Quitting the application. Goodbye!");
+                break;
+            }
+        }
+    }
+}
